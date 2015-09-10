ada52dcbba1f040e7e7e36d631abc8219afd88bf
IwonaKr <iwona.k.lbn@gmail.com>
2015-09-10 19:53:30 +0200

    1st
    commin

---
+++ praca_magisterska/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using praca_magisterska.Resources;
+
+namespace praca_magisterska
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
